9d4c440ec282b7d0f7cf5d70bb1abcaf42e0f3d5
NAMATAMAGO314 <kyosuke.sato.314@gmail.com>
2025-03-31 15:47:28 +0900

    first commit

---
+++ Core/Src/robomas_kounai.hpp
@@ -0,0 +1,26 @@
+#include <main.h>
+
+class robomas{
+public:
+    float robomas_pwm_data[4];
+    int16_t robomas_int16_pwm_data[4]; //float to int
+    uint8_t robomas_send_data[8];
+
+    uint32_t *txmailbox;
+    CAN_TxHeaderTypeDef *txheader;
+    CAN_HandleTypeDef *can;
+
+    robomas(CAN_HandleTypeDef*,CAN_TxHeaderTypeDef*,uint32_t*);
+    void rotate(float robomas_pwm_data[4]);
+};
+struct RoboMaster {
+    int angle;
+    int velocity;
+    int torque;
+    int target;
+    float integral;
+    float derivative;
+    float Ki;
+    float Kp;
+    float Kd;
+};
